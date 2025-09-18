06574158a9a322b2804f76ad1549f480645a4311
BrettPost <bpost121@gmail.com>
2025-09-17 21:28:09 -0700

    Basic Drizzle configuration

---
+++ src/drizzle/schema/student.ts
@@ -0,0 +1,10 @@
+import { integer, varchar, pgTable, bigint } from "drizzle-orm/pg-core";
+import { teacher } from "./teacher"
+
+export const student = pgTable('student', {
+    id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+    firstName: varchar("first_name").notNull(),
+    lastName: varchar("last_name"),
+    grade: integer(),
+    teacherId: bigint("teacher_id", {mode: "number"}).references(() => teacher.id)
+})
---
+++ src/drizzle/schema/teacher.ts
@@ -0,0 +1,8 @@
+import { integer, varchar, pgTable, bigint } from "drizzle-orm/pg-core";
+
+export const teacher = pgTable('teacher', {
+    id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+    firstName: varchar("first_name"),
+    lastName: varchar("last_name").notNull(),
+    grade: integer()
+})
